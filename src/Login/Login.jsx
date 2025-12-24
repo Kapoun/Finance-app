f1160ccb933f33170dd047d78a8d96d1609a44ee
kapoun <123989810+Kapoun@users.noreply.github.com>
2025-12-24 05:54:49 +0300

    update on the login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -8,23 +8,37 @@ import logo from '../assets/logo.png';
 
 function Login() {
 const [username , setUsername] = useState("")
+const [password , setPassword] = useState("")
 
-function username(e){
+function userdata(e){
   setUsername(e.target.value)
+  
 }
 
+function passworddata(e){
+  setPassword(e.target.value)
+  
+}
+
+function onSubmit(e){
+  e.preventDefault();
+}
+
+ 
   return (
+
     <div>
         <div className='login-container'>
           <img src={logo} alt="Finance Tracker Logo" className='logo' />
           <h2>Login Page</h2>
             <p>Welcome back loging to continue where you left</p> 
           <form >
-            <label>Username</label>
-            <input type="email" placeholder='Enter username' value={username} />
-            <label>Password</label>
-            <input type="password" placeholder='Enter Password' />
-            <button>Login</button>
+            <label>Username {username}</label>
+            <input type="email" placeholder='Enter username' value={username} onChange={userdata} />
+            <label>Password {password}</label>
+            <input type="password" placeholder='Enter Password' value={password} onChange={passworddata} />
+
+            <button type="submit" onSubmit={onSubmit}>Login</button>
             <label>If you don't have an account</label>
             <Link to="/register">Register Here</Link>
            </form>  
--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -14,7 +14,25 @@ function Register() {
     "Investments",
     "Settings",
   ];
+}
 
+function registerUser(e){
+
+  const [user , SetUser] = React.useState({
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: ""
+  });
+}
+
+  function handleChange(e){
+    const {name , value} = e.target;
+    SetUser(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
 
   return (
     <div className='register-container'>
@@ -23,20 +41,20 @@ function Register() {
 
       <h2>Register Page</h2>
 
-      <label>Full Name</label>
-      <input type="name" placeholder='Enter full name' />
+      <label>Full Name {user.fullName}</label>
+      <input type="name" value={user.fullName}  placeholder='Enter full name' onChange={handleChange} />
 
-      <label>Email</label>
-      <input type="email" placeholder='Enter email' />
+      <label>Email {user.email}</label>
+      <input type="email" value={user.email} placeholder='Enter email' onChange={handleChange}/>
            
-      <label>Phone nummber</label>
-      <input type="number" placeholder='Enter phone number' />
+      <label>Phone nummber {user.phoneNumber}</label>
+      <input type="number" value={user.phoneNumber} placeholder='Enter phone number' onChange={handleChange} />
 
-      <label>Password</label>
-      <input type="password" placeholder='Enter Password' />
+      <label>Password {user.password}</label>
+      <input type="password" value={user.password} placeholder='Enter Password' onChange={handleChange}/>
 
-      <label>confirm Password</label>
-      <input type="password" placeholder='Enter Password' />
+      <label>confirm Password {user.confirmPassword}</label>
+      <input type="password" value={user.confirmPassword} placeholder='Enter Password' onChange={handleChange} />
 
       <button>Register</button>
 
